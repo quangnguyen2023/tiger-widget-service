4357d1c7c4e1aa5d9660e49adfc4366f373abef8
Quang <nmquang2012@gmail.com>
2025-06-18 19:07:16 +0700

    feat: configure global environment variables and enable CORS in main application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WidgetsModule } from './widgets/widgets.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    WidgetsModule,
+    ConfigModule.forRoot({ isGlobal: true }), // Load environment variables globally
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
