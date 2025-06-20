51e42ba014bee4c637e135027a1871863173fcd3
Quang <nmquang2012@gmail.com>
2025-06-21 01:04:03 +0700

    feat( health ): Add Health Controller which allow UptimeRobot to ping to web server

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WidgetsModule } from './widgets/widgets.module';
 import { ConfigModule } from '@nestjs/config';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [
     WidgetsModule,
     ConfigModule.forRoot({ isGlobal: true }), // Load environment variables globally
   ],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
