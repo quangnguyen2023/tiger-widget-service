d38c5035c41485b78c97c54ad3df4ae45c46d7e4
Quang <nmquang2012@gmail.com>
2025-06-20 13:22:13 +0700

    refactor (db): specify column names for better mapping

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,7 +4,7 @@ export const widgetsTable = pgTable('widgets', {
   id: varchar({ length: 50 }).primaryKey(),
   owner: varchar({ length: 5 }).notNull(),
   type: varchar({ length: 20 }).notNull(),
-  customValues: jsonb().notNull(),
-  createdAt: timestamp().defaultNow().notNull(),
+  customValues: jsonb('custom_values').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
   name: varchar({ length: 50 }),
 });
