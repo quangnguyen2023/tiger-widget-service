5ac8a759810c96e4c82d4ca7dbffc1eb515ee895
Quang <nmquang2012@gmail.com>
2025-07-14 16:39:05 +0700

    refactor(auth): simplify user object return in getUser method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -34,7 +34,7 @@ export class AuthService {
     }
 
     const { password: _, ...userWithoutPassword } = user;
-    return { user: userWithoutPassword };
+    return userWithoutPassword;
   }
 
   async signUp({ name, email, password }: SignUpDto) {
